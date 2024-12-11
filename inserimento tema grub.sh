ea8cf9030b8a26d4de9b5f9bb705d34fed6561e4
FC3243D4 <colacicchi4@gmail.com>
2024-12-11 11:22:48 +0100

    creazione
    aggiunta script per l'inserimento di un tema di grub

---
+++ inserimento tema grub.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+test "$IN_TERM" || {
+    export IN_TERM=1
+    konsole --hold -e "$0"
+    exit 0
+} && true
+
+sudo nano /etc/default/grub
+sudo grub2-mkconfig -o /etc/grub2-efi.cfg
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+sudo sed -i -e "s/'UEFI Firmware Settings'/'BIOS' --class uefi-firmware/" "/boot/grub2/grub.cfg"
+sudo nano /boot/grub2/grub.cfg
+kill -9 $PPID
