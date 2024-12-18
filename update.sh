9b3aeed9407c84625c7afe7cea30e9d8c8a6f034
FC3243D4 <colacicchi4@gmail.com>
2024-12-18 12:18:12 +0100

    added check for kernel update

--- update.sh
+++ update.sh
@@ -5,5 +5,16 @@ test "$IN_TERM" || {
     exit 0
 } && true
 
-sudo dnf update && sudo grub2-mkconfig -o /boot/grub2/grub.cfg && kill -9 $PPID
+sudo dnf update > check-kernel.temp
+
+if grep -q "kernel-core" "check-kernel.temp"; then
+    sudo grub-customizer
+    sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+    sudo sed -i -e "s/'UEFI Firmware Settings'/'BIOS' --class uefi-firmware/" "/boot/grub2/grub.cfg"
+    sudo nano /boot/grub2/grub.cfg
+fi
+
+sudo rm check-kernel.temp
+kill -9 $PPID
+
 
