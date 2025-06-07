6a3af802de25db9faac2de33dafc38708ac62610
FC3243D4 <FC3243D4@users.noreply.github.com>
2025-06-07 18:47:16 +0200

    corrected scripts

--- install-latex.sh
+++ install-latex.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-yay -S texlive-installer
+#yay -S texlive-installer
+sudo pacman -S texlive-basic texlive-latex texlive-latexrecommended texlive-latexextra texlive-fontsrecommended texlive-fontsextra texlive-xetex texlive-luatex texlive-bibtexextra texlive-mathscience texlive-lang biber texlive-binextra perl-yaml-tiny perl-file-homedir
